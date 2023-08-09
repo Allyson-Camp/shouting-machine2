f3521c4966e6052da4df5216a24b1acac55fd106
Allyson Camp <Allysoncamp92@gmail.com>
2023-08-09 15:03:47 -0700

    add form.onsubmit function
    co-authored-by: Daisy Orozco <daisyyy.ob@gmail.com>
    Co-authored-by: Similoluwa Oyinkoalde <simi.oyinkolade@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+
+  const form = document.querySelector("form");
+  form.onsubmit = function (event) {
+    const userAnswer = document.querySelector("input#user-input").value;
+    document.querySelector("p#answer").innerText = userAnswer.toUpperCase();
+    event.preventDefault();
+  };
+
+};
